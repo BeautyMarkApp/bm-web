63902d1732d3bd4f03d501f98aa8888c45730ca9
Brittany Forks <brittanyforks@gmail.com>
2024-11-04 15:50:58 -0800

    Add relative path to image

--- src/components/landing/Selector.jsx
+++ src/components/landing/Selector.jsx
@@ -12,7 +12,7 @@ export default function Selector() {
       price: '$39.99',
     },
     'lifetime': {
-      price: '$99.99',
+      price: '$169.99',
     },
   };
 
@@ -22,7 +22,7 @@ export default function Selector() {
     <section aria-labelledby="pricing-section" id="pricing">
   <div class="lg:px-16 max-w-7xl md:px-12 mx-auto px-8 py-24 scroll-mt-12">
     <div class="text-center max-w-3xl mx-auto">
-      <img class="inline bg-center w-32 pb-8" src="/public/ProModeIcon-Light3x.png"/>
+      <img class="inline bg-center w-32 pb-8" src="./public/ProModeIcon-Light3x.png"/>
       <h2 class="text-4xl tracking-tight font-medium text-black md:text-5xl font-display">
         Take your beauty stash to the next level with BeautyMark Pro
       </h2>
@@ -32,8 +32,8 @@ export default function Selector() {
           type="button"
           onClick={() => setSelectedTab('monthly')}
           className={clsx(
-            'relative inline-flex items-center rounded-l-md  px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10',
-            selectedTab === 'monthly' ? 'bg-white text-gray-900 ring-gray-300 hover:bg-gray-50' : '',
+            'relative inline-flex items-center rounded-l-xl  px-3 py-2 text-sm font-semibold border-l-gray-300 ring-inset focus:z-10',
+            selectedTab === 'monthly' ? 'bg-pink-500 hover:bg-pink-600 text-white ring-gray-300 hover:bg-gray-50' : '',
             )}>
           Monthly
         </button>
@@ -41,8 +41,8 @@ export default function Selector() {
           type="button"
           onClick={() => setSelectedTab('yearly')}
           class={clsx(
-            'relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold  ring-1 ring-inset focus:z-10',
-            selectedTab === 'yearly' ? 'bg-white text-gray-900 ring-gray-300 hover:bg-gray-50' : '',
+            'relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10',
+            selectedTab === 'yearly' ? 'bg-pink-500 hover:bg-pink-600 text-gray-900 ring-gray-300 hover:bg-gray-50' : '',
           )}>
           Yearly
         </button>
@@ -50,8 +50,8 @@ export default function Selector() {
           type="button"
           onClick={() => setSelectedTab('lifetime')}
           class={clsx(
-            'relative -ml-px inline-flex items-center rounded-r-md  px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10',
-            selectedTab === 'lifetime' ? 'bg-white text-gray-900 ring-gray-300 hover:bg-gray-50' : '',
+            'relative -ml-px inline-flex items-center rounded-r-xl px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10',
+            selectedTab === 'lifetime' ? 'bg-pink-500 hover:bg-pink-600 text-gray-900 ring-gray-300 hover:bg-gray-50' : '',
           )}>
           Lifetime
         </button>
@@ -61,7 +61,7 @@ export default function Selector() {
     <div
       class="flex gap-4 items-start justify-center max-w-2xl py-4 mt-24 -mx-4 xl:-mx-12 sm:mx-auto lg:max-w-none"
     >
-      <section class="free flex flex-col order-first px-6 py-8 rounded-3xl sm:px-8 bg-gray-300 lg:order-none">
+      <section class="free flex flex-col order-first px-6 py-8 rounded-3xl sm:px-8 ring-1 ring-gray-400 lg:order-none">
         <h3 class="mt-5 text-lg text-black font-display">Standard</h3>
         <p class="mt-2 text-sm text-gray-500">
           Good for those managing just the essentials.
